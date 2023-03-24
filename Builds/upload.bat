c09d8d20caad6e640618c9c8cef6b2dfb68eddd8
Anguillifax <anguillifax@gmail.com>
2023-03-23 19:02:23 -0700

    Add Upload Script

--- Builds/upload.bat
+++ Builds/upload.bat
@@ -1,4 +1,14 @@
 @echo off
-echo (!) No itch.io page yet
+
+set suffix=
+
+CALL :Push win64
+@REM CALL :Push osx
+@REM CALL :Push web
 
 pause
+
+:Push
+echo Pushing folder `%~1` as %~1%suffix%
+butler push "%~1" sturmdesign/vestige:%~1%suffix%
+EXIT /B 0
