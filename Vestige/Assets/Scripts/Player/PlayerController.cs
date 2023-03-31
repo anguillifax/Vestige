23b1ef4766100a90754cd0064f060f56f3f40c07
Anguillifax <anguillifax@gmail.com>
2023-03-31 00:10:50 -0700

    Improve Fog, Add WavingLeaves Shader

--- Vestige/Assets/Scripts/Player/PlayerController.cs
+++ Vestige/Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,47 @@ using UnityEngine;
 
 namespace Vestige
 {
-	public class PlayerController
+	public class PlayerController : MonoBehaviour
 	{
+		[Header("Common")]
+		public Transform lookRotation;
+
+		[Header("Walk")]
+		public float walkVel = 8;
+		public float walkAccel = 30;
+
+		private Rigidbody body;
+
+		private void Awake()
+		{
+			body = GetComponent<Rigidbody>();
+
+			if (!lookRotation)
+			{
+				lookRotation = transform;
+			}
+		}
+
+		private void FixedUpdate()
+		{
+			Vector3 vel = body.velocity;
+			vel.y = 0;
+
+			Vector3 inputs = new Vector3(
+				Input.GetAxisRaw("Horizontal"),
+				0,
+				Input.GetAxisRaw("Vertical"));
+			inputs = Vector3.ClampMagnitude(inputs, 1);
+
+			Vector3 rotated = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0) * inputs;
+
+			vel = Vector3.MoveTowards(
+				vel,
+				rotated * walkVel,
+				walkAccel * Time.fixedDeltaTime);
+
+			vel.y = body.velocity.y;
+			body.velocity = vel;
+		}
 	}
 }
