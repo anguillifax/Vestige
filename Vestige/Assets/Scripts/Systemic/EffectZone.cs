111aa159a75ad313ef61cecf71a168d12de95d5a
Anguillifax <anguillifax@gmail.com>
2023-05-07 22:37:34 -0700

    Add Effect Zone

---
+++ Vestige/Assets/Scripts/Systemic/EffectZone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vestige
+{
+	public class EffectZone : MonoBehaviour
+	{
+		// =========================================================
+		// Variables
+		// =========================================================
+
+		[Header("Common")]
+		[Expandable] public SystemicEffectTemplate effect;
+
+		// =========================================================
+		// Physics Callbacks
+		// =========================================================
+
+		private void OnTriggerStay(Collider other)
+		{
+			if (other.attachedRigidbody)
+			{
+				ApplyEffect(other.attachedRigidbody.gameObject);
+			}
+		}
+
+		private void ApplyEffect(GameObject root)
+		{
+			IRecipient systemic = root.GetComponent<IRecipient>();
+			if (systemic != null)
+			{
+				systemic.RecieveEffect(effect.AsEffect(gameObject));
+			}
+		}
+	}
+}
