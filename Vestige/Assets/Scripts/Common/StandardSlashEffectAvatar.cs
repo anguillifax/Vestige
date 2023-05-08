189e3740aa6f5ddc63a3a9a0af45b2d9e95d86fd
Anguillifax <anguillifax@gmail.com>
2023-05-07 22:19:37 -0700

    Add Torch Swipe, Add Torch Throw, Add Layers

---
+++ Vestige/Assets/Scripts/Common/StandardSlashEffectAvatar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Vestige
+{
+	public class StandardSlashEffectAvatar : MonoBehaviour
+	{
+		[Header("Common")]
+		public UnityEvent started;
+
+		[Header("Debug")]
+		public InspectorCallbackButton testBegin = new InspectorCallbackButton("Begin Slash");
+
+		private void Awake()
+		{
+			testBegin.callback = BeginSlash;
+		}
+
+		public void BeginSlash()
+		{
+			started.Invoke();
+		}
+	}
+}
