26d183dbaf1341a74f08b585c5f71f44771cd1cb
Bryan Sturm <anguillifax@gmail.com>
2023-05-21 15:40:15 -0700

    Add Thrown Torches Ignite

--- Vestige/Assets/Scripts/Props/TorchController.cs
+++ Vestige/Assets/Scripts/Props/TorchController.cs
@@ -13,7 +13,7 @@ namespace Vestige
 
 		public enum State
 		{
-			Idle, Swipe, Cooldown,
+			Idle, Swipe, Cooldown, Thrown,
 		}
 
 		// =========================================================
@@ -33,6 +33,8 @@ namespace Vestige
 		public float throwAngle = 30f;
 		public float throwVelocity = 6f;
 		public float inheritFactor = 1;
+		public ManualTimer throwBurningTimer = new ManualTimer(2);
+		public SystemicEffectTemplate throwEffect;
 
 		private State state;
 		private StandardHoldable holdable;
@@ -59,21 +61,24 @@ namespace Vestige
 
 		private void Update()
 		{
+			UpdateState();
 			if (holdable.Active) UpdateHoldable();
 
 			cooldown.Update(Time.deltaTime);
 			swipeDuration.Update(Time.deltaTime);
+			throwBurningTimer.Update(Time.deltaTime);
 		}
 
-		private void UpdateHoldable()
+		private void UpdateState()
 		{
 			switch (state)
 			{
 				case State.Idle:
-					if (holdable.input.PrimaryDown)
+					if (holdable.Active && holdable.input.PrimaryDown)
 					{
 						swipeDuration.Start();
 						swipeStarted.Invoke();
+						throwBurningTimer.Stop();
 						state = State.Swipe;
 					}
 					break;
@@ -92,8 +97,18 @@ namespace Vestige
 						state = State.Idle;
 					}
 					break;
-			}
 
+				case State.Thrown:
+					if (throwBurningTimer.Done)
+					{
+						state = State.Idle;
+					}
+					break;
+			}
+		}
+
+		private void UpdateHoldable()
+		{
 			if (holdable.input.SecondaryDown)
 			{
 				Vector3 localDir = Quaternion.Euler(-throwAngle, 0, 0) * Vector3.forward;
@@ -108,22 +123,33 @@ namespace Vestige
 				}
 
 				holdable.harness.Detach();
-				state = State.Cooldown;
 
 				GetComponent<Rigidbody>().velocity = vel;
+
+				throwBurningTimer.Start();
+				state = State.Thrown;
 			}
 		}
 
 		private void OnTriggerStay(Collider other)
 		{
-			if (state == State.Swipe && other.attachedRigidbody != null)
+			switch (state)
 			{
-				IRecipient systemic = other.attachedRigidbody.GetComponent<IRecipient>();
-				if (systemic != null)
-				{
-					Effect e = swipeEffect.AsEffect(gameObject);
-					systemic.RecieveEffect(e);
-				}
+				case State.Swipe:
+					SystemicUtil.BroadcastIfExists(swipeEffect, gameObject, other);
+					break;
+
+				case State.Thrown:
+					SystemicUtil.BroadcastIfExists(throwEffect, gameObject, other);
+					break;
+			}
+		}
+
+		private void OnCollisionEnter(Collision collision)
+		{
+			if (throwBurningTimer.Running)
+			{
+				throwBurningTimer.Start();
 			}
 		}
 	}
