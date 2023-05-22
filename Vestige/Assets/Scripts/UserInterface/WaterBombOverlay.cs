ab3cb87f65c768df31612433a3d75aa0142e7a4f
Bryan Sturm <anguillifax@gmail.com>
2023-05-22 12:19:02 -0700

    Add Water Bomb, Add RadialExplosion

---
+++ Vestige/Assets/Scripts/UserInterface/WaterBombOverlay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Vestige
+{
+	public class WaterBombOverlay : MonoBehaviour
+	{
+		public UnityEvent activated;
+
+		public void SetHasActivated()
+		{
+			activated.Invoke();
+		}
+	}
+}
