c02f217301dc1e5db3d609cdd4482722b54605a3
Jessica <93614110+Jessica-200@users.noreply.github.com>
2023-05-24 14:42:21 -0700

    changes

---
+++ Vestige/Assets/Prototype/Jessica/WaterBombExplosion.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vestige.Prototype
+{
+    public class WaterBombExplosion : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
