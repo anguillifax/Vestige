296b7e11fd36d24e8671838acd64bc6f363bd4eb
alexdoant <alexanderrdoan@gmail.com>
2023-05-24 14:32:15 -0700

    finish explosive barrel avatar

--- Vestige/Assets/Scripts/Props/ExplosiveBarrelAvatar.cs
+++ Vestige/Assets/Scripts/Props/ExplosiveBarrelAvatar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Vestige.Prototype
+namespace Vestige
 {
     public class ExplosiveBarrelAvatar : MonoBehaviour
     {
