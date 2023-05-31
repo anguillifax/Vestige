2408696016da7da75fe32623c2c9229e65f31bba
lucasXiaoFan <xiaofl14@uci.edu>
2023-05-30 22:26:39 -0700

    AI

--- Vestige/Assets/Scripts/AI/EnemyManager.cs
+++ Vestige/Assets/Scripts/AI/EnemyManager.cs
@@ -10,6 +10,12 @@ namespace Vestige
 		EnemyAnim anim;
 		EnemyLocomotion locomotion;
 		public PlayerController player;
+		[Header("GamePlay")]
+		[SerializeField] Transform walkStraightDestination;
+		[SerializeField] Transform waterDestination;
+		[SerializeField] float randomFleeRadius = 100.0f;
+		[SerializeField] float timeBetweenMoves = 1.0f;
+		[SerializeField] float timer;
 
 		[Header("AI Setting")]
 		public float detectionRadius = 20f;
@@ -47,24 +53,83 @@ namespace Vestige
 			locomotion = GetComponent<EnemyLocomotion>();
 			player = FindObjectOfType<PlayerController>();
 			offset = Random.Range(0, 10);
+			timer = timeBetweenMoves;
 		}
 
 		void Update()
 		{
 			// HARDCODED
-			offset = (offset + 1) % 10;
-			if (offset == 0 && Vector3.Distance(player.transform.position, transform.position) < 10)
-			{
-				currentTarget = player.transform;
-			}
+			// offset = (offset + 1) % 10;
+			// if (offset == 0 && Vector3.Distance(player.transform.position, transform.position) < 10)
+			// {
+			// 	currentTarget = player.transform;
+			// }
 
 			isInteracting = anim.anim.GetBool("isInteracting");
-			locomotion.ChasePlayer();
-			locomotion.FacePlayer();
-			if (locomotion.CanDealDamage())
+			if (isDead)
 			{
-				anim.ApplyTargetAnimation("attack_02", true);
+				agent.enabled = false;
+				//play death animation
+				return;
 			}
+			else if (!isOnFire && walkStraightDestination == null)
+			{
+				locomotion.ChasePlayer();
+				locomotion.FacePlayer();
+				if (locomotion.CanDealDamage())
+				{
+					anim.ApplyTargetAnimation("attack_02", true);
+				}
+			}
+			else if (isOnFire && walkStraightDestination == null)
+			{
+				//walkstraight will not effect by anything
+				OnFire(waterDestination);
+
+			}
+			else if (walkStraightDestination)
+			{
+
+				WalkStraight(walkStraightDestination);
+			}
+
 		}
+		void WalkStraight(Transform destination)
+		{
+			if (Vector3.Distance(destination.position, transform.position) < 3f)
+			{
+				anim.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+			}
+			else
+			{
+				anim.anim.SetFloat("Vertical", 1, 0.1f, Time.deltaTime);
+			}
+			agent.enabled = true;
+			agent.SetDestination(destination.position);
+		}
+		void OnFire(Transform waterPosition)
+		{
+			agent.enabled = false;
+		}
+		// private void WalkRandomly()
+		// {
+		// 	timer += Time.deltaTime;
+		// 	if (timer >= timeBetweenMoves)
+		// 	{
+		// 		Vector3 newPosition = RandomNavSphere(transform.position, randomFleeRadius, -1);
+
+		// 		agent.SetDestination(newPosition);
+		// 		timer = 0;
+		// 	}
+		// }
+
+		// public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask)
+		// {
+		// 	Vector3 randomDirection = Random.insideUnitSphere * distance;
+		// 	randomDirection += origin;
+		// 	NavMeshHit navHit;
+		// 	NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+		// 	return navHit.position;
+		// }
 	}
 }
