a80d516d63ef86116b325c72194fa53cfaa7d0fd
Bryan Sturm <anguillifax@gmail.com>
2023-06-04 20:36:48 -0700

    Add Finish Game Scene

--- Vestige/Assets/Prototype/Ryan/Reset.cs
+++ Vestige/Assets/Prototype/Ryan/Reset.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Vestige.Prototype
 {
     public class Reset : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
         void Update()
         {
 			if (Input.GetKeyDown(KeyCode.P))
 			{
-				Application.LoadLevel(0);
+				SceneManager.LoadScene(0);
 			}
         }
     }
