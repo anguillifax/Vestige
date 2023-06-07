922f93efa5fe6da0f2acd0f5c4cc7489eacf65f1
Bryan Sturm <anguillifax@gmail.com>
2023-06-06 21:48:29 -0700

    Add Sprite Package, Add Holdable Pip

---
+++ Vestige/Assets/Scripts/Holdables/HoldablePipAvatar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Vestige
+{
+	public class HoldablePipAvatar : MonoBehaviour
+	{
+		[Header("Common")]
+		public Canvas canvas;
+		public Animator anim;
+		public Image uiImage;
+
+		private void Awake()
+		{
+			canvas.worldCamera = Camera.main;
+		}
+
+		private void LateUpdate()
+		{
+			transform.rotation = Quaternion.identity;
+		}
+
+		// =========================================================
+		// Public Interface
+		// =========================================================
+
+		public void SetOpen(bool shown)
+		{
+			anim.SetBool("Visible", shown);
+		}
+
+		public void SetVisible(bool shown)
+		{
+			uiImage.enabled = shown;
+		}
+	}
+}
---
+++ Vestige/Assets/Scripts/Holdables/HoldablePipController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Vestige
+{
+	public class HoldablePipController : MonoBehaviour
+	{
+		private HoldablePipAvatar avatar;
+
+		private void Awake()
+		{
+			avatar = GetComponent<HoldablePipAvatar>();
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				avatar.SetOpen(true);
+			}
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				avatar.SetOpen(false);
+			}
+		}
+
+		// =========================================================
+		// Public Interface
+		// =========================================================
+
+		public void SetShown(bool canShow)
+		{
+			avatar.SetVisible(canShow);
+		}
+	}
+}
